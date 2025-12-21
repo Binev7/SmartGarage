55b2f8862a7c302432de671dc0f1afe277b10e50
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-21 22:38:26 +0200

    feat: add AuthServiceImpl

---
+++ src/main/java/com/portfolio/smartgarage/service/AuthServiceImpl.java
@@ -0,0 +1,54 @@
+package com.portfolio.smartgarage.service;
+
+import com.portfolio.smartgarage.dto.LoginRequest;
+import com.portfolio.smartgarage.dto.RegisterRequest;
+import com.portfolio.smartgarage.mapper.UserMapper;
+import com.portfolio.smartgarage.model.Role;
+import com.portfolio.smartgarage.model.User;
+import com.portfolio.smartgarage.repository.UserRepository;
+import com.portfolio.smartgarage.security.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public void register(RegisterRequest request) {
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+
+        User user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.CUSTOMER);
+
+        userRepository.save(user);
+    }
+
+    @Override
+    public String login(LoginRequest request) {
+
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.getEmail(),
+                                request.getPassword()
+                        )
+                );
+
+        return jwtUtils.generateToken(authentication.getName());
+    }
+}
