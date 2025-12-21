196c9890af28eb88cf57bb4fd42b8f7d6c5aa879
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-21 18:34:07 +0200

    initial commit

---
+++ src/test/java/com/portfolio/smartgarage/SmartGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.smartgarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
