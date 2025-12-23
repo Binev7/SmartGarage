30b5b15b5b0f1f99f0be42849e1c732f0ac7316b
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-23 20:22:49 +0200

    feat: add vehicle dto's

---
+++ src/main/java/com/portfolio/smartgarage/dto/VehicleResponseDto.java
@@ -0,0 +1,22 @@
+package com.portfolio.smartgarage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VehicleResponseDto {
+    private Long id;
+    private String licensePlate;
+    private String vin;
+    private int year;
+    private String model;
+    private String brand;
+    private Long ownerId;
+    private String ownerEmail;
+}
+
