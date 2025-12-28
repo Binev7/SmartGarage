d6116939798133c7e09cf112aa69e92940d260db
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-28 19:18:04 +0200

    refactor: add Vehicle dto's to vehicle package

--- src/main/java/com/portfolio/smartgarage/dto/vehicle/VehicleRequestDto.java
+++ src/main/java/com/portfolio/smartgarage/dto/vehicle/VehicleRequestDto.java
@@ -1,22 +1,29 @@
-package com.portfolio.smartgarage.dto;
+package com.portfolio.smartgarage.dto.vehicle;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class VehicleResponseDto {
-    private Long id;
+public class VehicleRequestDto {
+
+    @NotBlank
     private String licensePlate;
+
+    @NotBlank
+    @Size(min = 17, max = 17)
     private String vin;
+
+    @Min(1887)
     private int year;
+
+    @NotBlank
     private String model;
+
+    @NotBlank
     private String brand;
-    private Long ownerId;
-    private String ownerEmail;
 }
 
