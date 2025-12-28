357d183b8380d54f0e397199ed0f9f72fd4ca00c
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-28 16:33:38 +0200

    feat: add VisitRepository

--- src/main/java/com/portfolio/smartgarage/repository/VisitRepository.java
+++ src/main/java/com/portfolio/smartgarage/repository/VisitRepository.java
@@ -2,16 +2,25 @@ package com.portfolio.smartgarage.repository;
 
 import com.portfolio.smartgarage.model.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.time.LocalDate;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
 
+    List<Visit> findAllByUserId(Long userId);
+
     List<Visit> findAllByVehicleId(Long vehicleId);
 
-    List<Visit> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+    boolean existsByVehicleId(Long vehicleId);
 
-    long countByDate(LocalDate date);
+    @Modifying
+    @Transactional
+    @Query("delete from Visit v where v.vehicle.id = :vehicleId")
+    void deleteAllByVehicleId(@Param("vehicleId") Long vehicleId);
 }
