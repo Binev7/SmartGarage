c53ae4b36952f76cde68d3c7ee506eed003172ac
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-28 18:25:59 +0200

    feat: add ServiceMapper

---
+++ src/main/java/com/portfolio/smartgarage/mapper/ServiceMapper.java
@@ -0,0 +1,26 @@
+package com.portfolio.smartgarage.mapper;
+
+import com.portfolio.smartgarage.dto.ServiceRequestDto;
+import com.portfolio.smartgarage.dto.ServiceResponseDto;
+import com.portfolio.smartgarage.model.Service;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceMapper {
+
+    public Service toEntity(ServiceRequestDto dto) {
+        return Service.builder()
+                .name(dto.getName())
+                .price(dto.getPrice())
+                .build();
+    }
+
+    public ServiceResponseDto toDto(Service service) {
+        return ServiceResponseDto.builder()
+                .id(service.getId())
+                .name(service.getName())
+                .price(service.getPrice())
+                .build();
+    }
+}
+
