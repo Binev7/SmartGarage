99068f8faa75473bcc8408a1941eac5d5f130842
Binev7 <georgi_binev.gogo@abv.bg>
2026-01-09 14:03:50 +0200

    feat: add convert method

--- src/main/java/com/portfolio/smartgarage/service/interfaces/CurrencyService.java
+++ src/main/java/com/portfolio/smartgarage/service/interfaces/CurrencyService.java
@@ -3,5 +3,8 @@ package com.portfolio.smartgarage.service.interfaces;
 import java.math.BigDecimal;
 
 public interface CurrencyService {
+
     BigDecimal getExchangeRate(String from, String to);
+
+    BigDecimal convert(BigDecimal amount, String from, String to);
 }
