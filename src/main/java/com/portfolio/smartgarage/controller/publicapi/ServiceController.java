c1a1262bb656fe497da4d59c8f90dd1166313aa2
Binev7 <georgi_binev.gogo@abv.bg>
2026-01-12 11:59:18 +0200

    refactor: add shared controllers for employee and customer

---
+++ src/main/java/com/portfolio/smartgarage/controller/publicapi/ServiceController.java
@@ -0,0 +1,29 @@
+package com.portfolio.smartgarage.controller.publicapi;
+
+import com.portfolio.smartgarage.dto.service.ServiceResponseDto;
+import com.portfolio.smartgarage.security.annotation.IsEmployeeOrCustomer;
+import com.portfolio.smartgarage.service.interfaces.ServiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/services")
+@IsEmployeeOrCustomer
+@RequiredArgsConstructor
+public class ServiceController {
+
+    private final ServiceService serviceService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ServiceResponseDto> getServiceById(@PathVariable Long id) {
+        return ResponseEntity.ok(serviceService.getServiceById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ServiceResponseDto>> getAllAvailableServices() {
+        return ResponseEntity.ok(serviceService.getAllServices());
+    }
+}
