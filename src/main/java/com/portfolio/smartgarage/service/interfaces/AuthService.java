670a272320faa754680d1a70c3836107cf3c68e0
Binev7 <georgi_binev.gogo@abv.bg>
2026-01-19 19:49:00 +0200

    refactor: add new logic to auth

--- src/main/java/com/portfolio/smartgarage/service/interfaces/AuthService.java
+++ src/main/java/com/portfolio/smartgarage/service/interfaces/AuthService.java
@@ -3,6 +3,7 @@ package com.portfolio.smartgarage.service.interfaces;
 import com.portfolio.smartgarage.dto.auth.AuthResponseDto;
 import com.portfolio.smartgarage.dto.auth.LoginRequestDto;
 import com.portfolio.smartgarage.dto.auth.RegisterRequestDto;
+import com.portfolio.smartgarage.dto.auth.ResetPasswordRequestDto;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +12,10 @@ public interface AuthService {
     void register(RegisterRequestDto request);
 
     AuthResponseDto login(LoginRequestDto request);
+
+    void forgotPassword(String email);
+
+    AuthResponseDto resetPassword(ResetPasswordRequestDto request);
+
 
 }
