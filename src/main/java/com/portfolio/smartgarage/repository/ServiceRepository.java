22252559f3491a72be18d3c9da4800ee49a529ed
Binev7 <georgi_binev.gogo@abv.bg>
2026-01-29 22:23:15 +0200

    chore: remove unnecessary annotation

--- src/main/java/com/portfolio/smartgarage/repository/ServiceRepository.java
+++ src/main/java/com/portfolio/smartgarage/repository/ServiceRepository.java
@@ -3,7 +3,6 @@ package com.portfolio.smartgarage.repository;
 import com.portfolio.smartgarage.model.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
