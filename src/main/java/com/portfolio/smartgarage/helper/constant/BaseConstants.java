9d75a2651a8e4b32baf9b55fe1339c0070173120
Binev7 <georgi_binev.gogo@abv.bg>
2026-01-29 22:19:03 +0200

    feat: add CustomerVisitControllerMvc

--- src/main/java/com/portfolio/smartgarage/helper/constant/BaseConstants.java
+++ src/main/java/com/portfolio/smartgarage/helper/constant/BaseConstants.java
@@ -2,6 +2,8 @@ package com.portfolio.smartgarage.helper.constant;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.List;
+
 @UtilityClass
 public final class BaseConstants {
 
@@ -9,4 +11,7 @@ public final class BaseConstants {
     public static final int CALENDAR_DAYS_HORIZON = 14;
     public static final String BASE_CURRENCY = "BGN";
     public static final String SYSTEM_PASS_TEMPLATE = "[System] Generated Password: %s";
+    public static final List<String> WORK_HOURS = List.of(
+            "09:00", "10:00", "11:00", "13:00", "14:00", "15:00", "16:00"
+    );
 }
